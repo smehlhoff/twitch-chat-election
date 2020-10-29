998f98cd99c138f34b2c16eac8a19694132b79dc
smehlhoff <stevemehlhoff@gmail.com>
2020-10-29 12:06:15 -0700

    Change variable name

--- top_users.py
+++ top_users.py
@@ -8,9 +8,9 @@ df = pd.read_csv("./misc/chanlog.csv")
 c = Counter(df["username"]).most_common(10)
 
 usernames = [item[0] for item in c]
-freq = [item[1] for item in c]
+lines = [item[1] for item in c]
 
-plt.barh(usernames, freq)
+plt.barh(usernames, lines)
 plt.xlabel("Number of lines")
 plt.ylabel("Username")
 plt.title("Top 10 most active users")
