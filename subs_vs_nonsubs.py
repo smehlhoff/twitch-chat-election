6de272d9bf1693d6f510e96c651b4802c83cf716
smehlhoff <stevemehlhoff@gmail.com>
2020-10-29 12:03:17 -0700

    Count all rows by subs and nonsubs

--- subs_vs_nonsubs.py
+++ subs_vs_nonsubs.py
@@ -3,12 +3,12 @@ import pandas as pd
 
 df = pd.read_csv("./misc/chanlog.csv")
 
-users = df.groupby(["sub_count"])["username"].nunique()
+nonsubs = df.loc[df["sub_count"] == 0]
+subs = df.loc[df["sub_count"] > 0]
 
-nonsubs = users[:1].sum()
-subs = users[1:].sum()
+sizes = [len(nonsubs), len(subs)]
 
-plt.pie([nonsubs, subs], autopct="%1.1f%%", explode=(0.1, 0), wedgeprops={"edgecolor": "black"})
+plt.pie(sizes, autopct="%1.1f%%", explode=(0.1, 0), wedgeprops={"edgecolor": "black"})
 plt.axis("equal")
 plt.title("Which group spoke more frequently?")
 plt.legend(labels=["Non-subscribers", "Subscribers"], loc=4)
