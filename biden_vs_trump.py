5ceed7d2197dc6691817f598edbfa7c475d2d84e
smehlhoff <stevemehlhoff@gmail.com>
2020-10-30 21:22:26 -0700

    Set up two pie charts for phrases and emotes

--- biden_vs_trump.py
+++ biden_vs_trump.py
@@ -3,23 +3,50 @@ import pandas as pd
 
 df = pd.read_csv("./misc/chanlog.csv")
 
-biden = []
-trump = []
+biden = 0
+trump = 0
 
 for msg in df["user_msg"]:
     msg = msg.lower().split()
 
     for word in msg:
         if "biden" in word:
-            biden.append(word)
-        elif "trump" in word and word != "ttrump":
-            trump.append(word)
+            biden += 1
+        elif "trump" in word:
+            trump += 1
 
-sizes = [len(biden), len(trump)]
+biden_emotes = 0
+trump_emotes = 0
 
-plt.pie(sizes, autopct="%1.1f%%", explode=(0.1, 0), wedgeprops={"edgecolor": "black"})
-plt.axis("equal")
-plt.title("Which candidate was mentioned more?")
-plt.legend(labels=["Biden", "Trump"], loc=4)
+_biden_emotes = ["MALARKEY"]
+_trump_emotes = ["CHYNA", "DonaldPls", "TTrump"]
+
+for msg in df["user_msg"]:
+    msg = msg.split()
+
+    for word in msg:
+        if word in _biden_emotes:
+            biden_emotes += 1
+        elif word in _trump_emotes:
+            trump_emotes += 1
+
+sizes = [biden, trump]
+sizes2 = [biden_emotes, trump_emotes]
+
+plt.style.use('fivethirtyeight')
+
+fig, (ax1, ax2) = plt.subplots(1, 2)
+fig.suptitle("Which candidate had more mentions?")
+
+ax1.pie(sizes, autopct="%1.1f%%", explode=(0.1, 0), wedgeprops={"edgecolor": "black"})
+ax1.axis("equal")
+ax1.title.set_text("Phrases")
+ax1.legend(labels=["Biden", "Trump"], loc=4)
+
+ax2.pie(sizes2, autopct="%1.1f%%", explode=(0.1, 0), wedgeprops={"edgecolor": "black"})
+ax2.axis("equal")
+ax2.title.set_text("Emotes")
+ax2.legend(labels=["Biden", "Trump"], loc=4)
+
 plt.tight_layout()
 plt.show()
