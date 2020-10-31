b92a468aa84ab52752520c366f50e42307eacf67
smehlhoff <stevemehlhoff@gmail.com>
2020-10-30 21:21:52 -0700

    Add fivethirtyeight style

--- subs_vs_nonsubs.py
+++ subs_vs_nonsubs.py
@@ -8,6 +8,7 @@ subs = df.loc[df["sub_count"] > 0]
 
 sizes = [len(nonsubs), len(subs)]
 
+plt.style.use('fivethirtyeight')
 plt.pie(sizes, autopct="%1.1f%%", explode=(0.1, 0), wedgeprops={"edgecolor": "black"})
 plt.axis("equal")
 plt.title("Which group spoke more frequently?")
