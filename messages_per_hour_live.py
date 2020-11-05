cb41200c170389adf985ee1aada7ac349c4a8d88
smehlhoff <stevemehlhoff@gmail.com>
2020-11-05 00:21:33 -0800

    Add messages per hour when stream was live

---
+++ messages_per_hour_live.py
@@ -0,0 +1,26 @@
+import math
+from statistics import mean
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+df = pd.read_csv("./misc/chanlog.csv")
+
+df["timestamp"] = pd.to_datetime(df["timestamp"])
+df["hour"] = df["timestamp"].dt.hour
+
+series = df.groupby(["hour"])["username"].count()
+series = series[11:]
+
+mean_val = mean(series)
+
+plt.plot(series, marker="o")
+plt.axhline(mean_val, color="red", linestyle="--")
+plt.locator_params(axis="both", integer=True)
+plt.xlabel("Hour")
+plt.ylabel("Message count")
+plt.title("Number of messages per hour")
+plt.legend(labels=["Messages", "Mean"], loc=4)
+plt.text(x=16, y=45000, s=f"{mean_val:.2f}", horizontalalignment="center")
+plt.tight_layout()
+plt.show()
